fce862dcb94fb851e78cd0c607fb9d30ca8806d6
SacS <sachinjagat.singh@gmail.com>
2021-07-01 21:28:58 +0300

    Initial project creation

---
+++ Landmark Remark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Landmark Remark
+//
+//  Created by Sachin Jagat on 30/06/2021.
+//  Copyright © 2021 Sachin Jagat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
